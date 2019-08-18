fd55754b45e4052e3ac5627a89031bbea104caf6
dominguezi10 <dominguezi10@unitec.edu>
2019-08-17 19:37:19 -0500

    Inicio Proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    /*user*/
+    this.state = {
+      user:null
+    };
+
+    this.InicioSesion = this.InicioSesion.bind(this);
+    this.Salir = this.Salir.bind(this);
+    this.Estado = this.Estado.bind(this);
+  }
+
+  /*ciclo de vida del user, esto es propio de react*/
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user =>{
+      this.setState({user});
+    });
+  }
+
+  /* para iniciar sesion*/
+  InicioSesion() {
+    const val = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(val)
+    .then(result => console.log('${result.user.email} inicio Sesion!'))
+    .catch(error => console.log('Error!! ${error.code}: ${error.message}'));
+  }
+
+  /* cerrar sesion*/
+  Salir(){
+    firebase.auth().signOut()
+    .then(result => console.log('${result.user.email} cerro sesion'))
+    .catch(error => console.log('Error!! ${error.code}: ${error.message}'));
+  }
+
+  /*evaluar si esta logueado*/
+  Estado(){
+    if(this.state.user){
+      return (
+        <div>
+          <p>Bienvenido {this.state.user.displayName} </p>
+          <button onClick={this.Salir}>Salir</button> 
+        </div>
+
+      );
+    }else{
+      return (<button onClick={this.InicioSesion}> Login </button>);
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          </header>
+
+          <p>Este es mi proyecto !!!!</p>
+          <p> {this.Estado()} </p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDOPhjnlVCT7MuquMGxdPQL0zlD1a3jm9E",
+    authDomain: "examen1ux-f9cde.firebaseapp.com",
+    databaseURL: "https://examen1ux-f9cde.firebaseio.com",
+    projectId: "examen1ux-f9cde",
+    storageBucket: "",
+    messagingSenderId: "361950530595",
+    appId: "1:361950530595:web:1040618732739c2a"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
