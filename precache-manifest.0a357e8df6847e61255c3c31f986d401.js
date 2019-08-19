bb6ab324333650fdaa775d3dd853617d13ab60c1
dominguezi10 <dominguezi10@unitec.edu>
2019-08-19 00:05:39 -0500

    Updates

---
+++ precache-manifest.0a357e8df6847e61255c3c31f986d401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b7c026c0e73a930808a2b86262e2d1",
+    "url": "/Examen1_UX/index.html"
+  },
+  {
+    "revision": "49bc8950e84999a92fe0",
+    "url": "/Examen1_UX/static/css/main.ac34b64d.chunk.css"
+  },
+  {
+    "revision": "23f2261ad03b352b53db",
+    "url": "/Examen1_UX/static/js/2.1ef14076.chunk.js"
+  },
+  {
+    "revision": "49bc8950e84999a92fe0",
+    "url": "/Examen1_UX/static/js/main.7bc4fcf2.chunk.js"
+  },
+  {
+    "revision": "8c0ecc58308065920f39",
+    "url": "/Examen1_UX/static/js/runtime~main.6a4570bb.js"
+  },
+  {
+    "revision": "a3c532be85eb8e92f8e3d90acadbba18",
+    "url": "/Examen1_UX/static/media/fondo.a3c532be.jpg"
+  }
+]);
